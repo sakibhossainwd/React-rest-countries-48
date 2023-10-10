e32dd263519de17b445247d99205e95a1857fdf8
sakibhossain <sakibhossainceo@gmail.com>
2023-10-10 07:31:13 -0700

    flags added

--- src/components/Country/country.js
+++ src/components/Country/country.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {area, region} = props.country;
+    const {area, region, flags} = props.country;
     return (
         <div className='country'>
             <h1>Name: {props.country.name.common}</h1>
             <h3>Capital: {props.country.capital}</h3>
             <h5>Area: {area}</h5>
             <h5>Region: {region}</h5>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
