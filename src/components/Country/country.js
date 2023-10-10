9e4592313794f7186fd52824459c09f301801b94
sakibhossain <sakibhossainceo@gmail.com>
2023-10-10 07:21:21 -0700

    object distructuring

--- src/components/Country/country.js
+++ src/components/Country/country.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    const {area, region} = props.country;
     return (
         <div className='country'>
             <h1>Name: {props.country.name.common}</h1>
             <h3>Capital: {props.country.capital}</h3>
-
+            <h5>Area: {area}</h5>
+            <h5>Region: {region}</h5>
         </div>
     );
 };
