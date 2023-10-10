f5756b5b390d443cf30adf30d40f91618557cd87
sakibhossain <sakibhossainceo@gmail.com>
2023-10-10 06:44:17 -0700

    sigle file added

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './Countries.css'
+import Country from '../Country/Country'
+
+
+
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
+    return (
+        <div>
+            <h2 className='heading'>This is another folder component: {countries.length}</h2>
+        {
+            countries.map(country => <Country name={country.name.common}></Country>)
+        }
+        </div>
+    );
+};
+
+export default Countries;
